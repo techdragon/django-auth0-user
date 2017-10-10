60e43e5f4ec41a94d1a4a52ac8217d5d131b357d
Samuel Bishop <sam@psyx.co>
2017-10-10 11:40:52 +0800

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='django-auth0-user',
-    version='0.4.0',
+    version='0.5.0',
     license='BSD license',
     description='Django Authentication and Authorisation using Auth0 and Python Social Auth',
     long_description='%s\n%s' % (
