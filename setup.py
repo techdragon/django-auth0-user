5939c1b0bd9ea000b292c36dece8f01b39427835
Samuel Bishop <sam@psyx.co>
2018-02-14 15:28:22 +0800

    Add explict dependency on open id connect extras.
    - Ensure that social core is always installed with the open id connect extras.

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     ],
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
-        'social-auth-core>=1.4.0',
+        'social-auth-core[openidconnect]>=1.4.0',
         'social-auth-app-django>=1.2.0',
         'django>=1.10',
         "cached-property>=1.3.0",
